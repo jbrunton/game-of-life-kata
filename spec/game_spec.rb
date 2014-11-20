17f2c1d4445cfcfc845dd91ba6c047df988e0b25
John Brunton <john_brunton@hotmail.co.uk>
2014-11-20 20:37:30 +0000

    basic survival rule implementation

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,6 +6,23 @@ class Game
   def active_counters
     @counters
   end
+  
+  def play
+    survivors = @counters.select do |counter|
+      neighbors = find_neighbors(counter)
+      neighbors.size == 2
+    end
+    @counters = survivors
+  end
+  
+private
+  def find_neighbors(counter)
+    @counters.select do |candidate|
+      delta_x = (candidate[:x] - counter[:x]).abs
+      delta_y = (candidate[:y] - counter[:y]).abs
+      delta_x ^ delta_y == 1
+    end
+  end
 end
 
 describe "Game" do
@@ -20,6 +37,41 @@ describe "Game" do
     it "lists the active counters" do
       game = Game.new([ {x: 1, y: 2} ])
       expect(game.active_counters).to eq([ {x: 1, y: 2} ])
+    end
+  end
+  
+  context "if the cell has no neighbours" do
+    before(:each) do
+      @game = Game.new([ {x: 0, y: 0} ])
+    end
+    
+    it "should die after a play" do
+      @game.play
+      expect(@game.active_counters).to be_empty
+    end
+  end
+  
+  context "survival rule" do
+    context "if the cell has 2 horizontal neighbors" do
+      before(:each) do
+        @game = Game.new([ {x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0} ])
+      end
+      
+      it "should survice after a play" do
+        @game.play
+        expect(@game.active_counters).to eq([ {x: 1, y: 0} ])
+      end
+    end
+    
+    context "if the cell has 2 vertical neighbors" do
+      before(:each) do
+        @game = Game.new([ {x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2} ])
+      end
+      
+      it "should survice after a play" do
+        @game.play
+        expect(@game.active_counters).to eq([ {x: 0, y: 1} ])
+      end
     end
   end
 end
