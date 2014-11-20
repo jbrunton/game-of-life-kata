a11f12aa5cb3c657ee113d84cd83b53452e9c37e
John Brunton <john_brunton@hotmail.co.uk>
2014-11-20 20:08:51 +0000

    initialize game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,10 @@
 class Game
   def initialize(counters)
+    @counters = counters
   end
   
   def active_counters
-    []
+    @counters
   end
 end
 
@@ -12,6 +13,13 @@ describe "Game" do
     it "has no active counters" do
       game = Game.new([])
       expect(game.active_counters).to be_empty
+    end
+  end
+  
+  context "when the board is initialized" do
+    it "lists the active counters" do
+      game = Game.new([ {x: 1, y: 2} ])
+      expect(game.active_counters).to eq([ {x: 1, y: 2} ])
     end
   end
 end
