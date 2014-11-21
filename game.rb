9f9ea7ff92e3ea5b8c77c6ef83ec8fed6371ffc2
John Brunton <john_brunton@hotmail.co.uk>
2014-11-21 14:31:50 +0000

    implemented survival rule

--- game.rb
+++ game.rb
@@ -8,6 +8,21 @@ class Game
   end
   
   def play
-    @active_cells = []
+    @active_cells = @active_cells.select do |cell|
+      neighbor_count(cell) >= 2
+    end
+  end
+  
+  def neighbor_count(cell)
+    @active_cells.select{ |active_cell| Game.are_neighbors(cell, active_cell) }.size
+  end
+  
+  def self.are_neighbors(cell1, cell2)
+    delta_x = (cell1[:x] - cell2[:x]).abs
+    delta_y = (cell1[:y] - cell2[:y]).abs
+    
+    (delta_x + delta_y) > 0 &&
+      delta_x <= 1 &&
+      delta_y <= 1
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,4 +15,46 @@ describe "Game" do
       expect(game.active_cells).to be_empty
     end
   end
+  
+  context ".are_neighbors" do
+    it "returns false if the cells aren't horizontal neighbors" do
+      expect(Game.are_neighbors({x: 0, y: 0}, {x: 2, y: 0})).to eq(false)
+    end
+    
+    it "returns false if the cells aren't vertical neighbors" do
+      expect(Game.are_neighbors({x: 0, y: 0}, {x: 0, y: 2})).to eq(false)
+    end
+    
+    it "returns false if the cells aren't diagonal neighbors" do
+      expect(Game.are_neighbors({x: 0, y: 0}, {x: 2, y: 2})).to eq(false)
+    end
+
+    it "returns true if the cells are horizontal neighbors" do
+      expect(Game.are_neighbors({x: 0, y: 0}, {x: 1, y: 0})).to eq(true)
+    end
+    
+    it "returns true if the cells are vertical neighbors" do
+      expect(Game.are_neighbors({x: 0, y: 0}, {x: 0, y: 1})).to eq(true)
+    end
+    
+    it "returns true if the cells are diagonal neighbors" do
+      expect(Game.are_neighbors({x: 0, y: 0}, {x: 1, y: 1})).to eq(true)
+    end
+    
+    it "returns false for the reflexive case" do
+      expect(Game.are_neighbors({x: 0, y: 0}, {x: 0, y: 0})).to eq(false)
+    end
+  end
+  
+  context "#neighbor_count" do
+    it "returns 0 for an empty board" do
+      game = Game.new([])
+      expect(game.neighbor_count({x: 0, y: 0})).to eq(0)
+    end
+    
+    it "returns the number of active neighbors" do
+      game = Game.new([ {x: 0, y: 1}, {x: 2, y: 1} ])
+      expect(game.neighbor_count({x: 1, y: 1})).to eq(2)
+    end
+  end
 end
