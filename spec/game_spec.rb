21e45560170412e08507256a6f97ddf3093fc7ba
John Brunton <john_brunton@hotmail.co.uk>
2014-11-21 10:40:13 +0000

    added unit test for Game#play method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,16 @@ require_relative File.expand_path('spec/spec_helper')
 describe "Game" do
   context "#initialize" do
     it "sets the initially active cells" do
-      game = Game.new([ {x: 1, y: 2} ])
-      expect(game.active_cells).to eq([ {x: 1, y: 2} ])
+      game = Game.new([ {x: 1, y: 1} ])
+      expect(game.active_cells).to eq([ {x: 1, y: 1} ])
+    end
+  end
+  
+  context "#play" do
+    it "applies a single round of the rules" do
+      game = Game.new([ x:1, y: 1])
+      game.play
+      expect(game.active_cells).to be_empty
     end
   end
 end
