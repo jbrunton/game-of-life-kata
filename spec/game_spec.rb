9d989da596e8791a991d3ebf9a01a1a0e0e119a5
John Brunton <john_brunton@hotmail.co.uk>
2014-11-21 09:25:07 +0000

    failing spec for underpopulation rule

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,6 +6,9 @@ class Game
   def active_cells
     @active_cells
   end
+  
+  def play
+  end
 end
 
 describe "Game" do
@@ -21,5 +24,53 @@ describe "Game" do
       game = Game.new([ {x: 1, y: 2} ])
       expect(game.active_cells).to eq([ {x: 1, y: 2} ])
     end
+  end
+  
+  context "underpopulation rule" do
+    context "if the cell has no neighbors" do
+      before(:each) do
+        initialize_board \
+          [0,0,0],
+          [0,1,0],
+          [0,0,0]
+        play
+      end
+      
+      it "should die after one play" do
+        expect_board \
+          [0,0,0],
+          [0,0,0],
+          [0,0,0]
+      end
+    end
+  end
+  
+  def initialize_board(*rows)
+    initial_cells = convert_to_cells(rows)
+    @game = Game.new(initial_cells)
+  end
+  
+  def play
+    @game.play
+  end
+  
+  def expect_board(*rows)
+    expected_cells = convert_to_cells(rows)
+    actual_cells = @game.active_cells.sort do |a,b|
+      result = a[:y] <=> b[:y]
+      result = a[:x] <=> b[:x] if result.zero?
+      result
+    end
+    expect(actual_cells).to eq(expected_cells)
+  end
+  
+  def convert_to_cells(rows)
+    cells = []
+    rows.each_index do |y|
+      rows[y].each_index do |x|
+        cells.push({x: x, y: (rows.length - 1) - y}) unless rows[y][x].zero?
+      end
+    end
+    cells
   end
 end
