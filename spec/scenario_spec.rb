f0d90f3e12bad029d71ff282dfaff06baa29459a
John Brunton <john_brunton@hotmail.co.uk>
2014-12-15 20:20:39 +0000

    scenario spec for overcrowding rule

--- spec/scenario_spec.rb
+++ spec/scenario_spec.rb
@@ -30,4 +30,18 @@ describe "Rules" do
         [0,1,0]
     end
   end
+  
+  describe "Overcrowding" do
+    it "kills cells with more than 3 live neighbors" do
+      initialize_board \
+        [0,1,0],
+        [1,1,1],
+        [0,1,0]
+      tick
+      expect_board \
+        [0,1,0],
+        [1,0,1],
+        [0,1,0]
+    end
+  end
 end
